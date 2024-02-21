75aa4285ffc622042fa42d09d730e1c4ab61a284
patriotkusuma <patriot.kusumasejati@gmail.com>
2024-02-21 22:20:25 +0700

    add page speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -55,6 +56,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
